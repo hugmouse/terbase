ea9ad8e98d8a20be746e86b16ba3702ef5953743
Mouse! <githubme@hugmouse.com>
2018-11-03 20:11:29 +0800

    Update func.php

--- functions/func.php
+++ functions/func.php
@@ -5,7 +5,7 @@ function timecheck($array)
     return (bool)strtotime($array);
 }
 
-// Для удаления лишних символов из строки (што)
+// Для удаления лишних символов из строки
 function trimmer($array) {
    // Исключаем из фильтра рузге алфавит, англицке навсякий ну и "-", ".", " "
    return preg_replace('/[^A-Za-z0-9А-Яа-я\-\s\.]/', '', $array); 
