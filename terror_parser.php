eb58faf0dd883f0b98e8101e3730432b4c8e8030
Mouse! <githubme@hugmouse.com>
2018-11-03 19:19:02 +0800

    Update terror_parser.php

--- terror_parser.php
+++ terror_parser.php
@@ -1,148 +1,64 @@
-<?php
-header('Content-Type: text/html; charset=utf8');
-
-// Чтобы хост не подумол, что мы роботы
-ini_set("user_agent","Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0");
-
-// Для парсинга HTML
-require 'libs/simple_html_dom.php';
-include 'functions/func.php';
-include 'mysql/database.php';
-
-//  "#russianFL" для загрузки листов
-$html = file_get_html('http://www.fedsfm.ru/documents/terrorists-catalog-portal-act#russianFL');
-
-// Для замера времени исполнения алгоритма
-$time_start = microtime(true); 
-
-// Находит все вхождения, то-есть список террористов
-// Так же страница подгружает и другие вкладки с одинаковыми классами, 
-// то начинаем поиск с 3 вхождения
-$html_ul = $html->find('ol[class=terrorist-list]', 3);
 
 
-foreach($html_ul->find('li') as $li) {
-    // /[^\,]+/ тоже возможно, но я захотел через пробелы :)
-    preg_match_all('/[^\s]+/', $li->plaintext, $word); 
-    $amount = count($word[0]);
+# RTBPA
+RTBPA = Russian Terrorist Base Parser Algorithm 
+Парсит список террористов, получаемый от "www.fedsfm.ru"
 
-    $item['ID']         = $word[0][0];
-    $item['name']       = $word[0][1];
-    $item['surname']    = $word[0][2];
-    $item['patronymic'] = $word[0][3];
+# Установка необходимого ПО
+Для установки используется либо HHVM, либо PHP7+, можно так же использовать вместе с PHPSH.
 
-    // Поиск исключений ввиде ФИО (ФИО). Бред.
-    if (preg_match_all("/[\(\)]/", $word[0][4], $word_clear, PREG_OFFSET_CAPTURE)) 
-    {
-        // ФИО(ФИО)
-        //    ^  ^ убираем эти тупые скобки
-        $clear_name_patronymic = str_replace(array( '(', ')'), '', array( $word[0][4], $word[0][6] ));
-        $item['name_second'] = $clear_name_patronymic[0];
-        $item['surname_second'] = $word[0][5];
-        $item['patronymic_second'] = $clear_name_patronymic[1];
-    }
-    else
-    {
-        if (preg_match_all("/(ОГЛЫ)|(УГЛИ)|(КЫЗЫ)|(УУЛУ)|(КИЗИ)/", $word[0][4], $word_clear, PREG_OFFSET_CAPTURE))
-        {
-            $item['date_of_birth'] = $word[0][5];
-            // i=8 ибо с 8 позиции в массиве начинается место проживания, это из-за указателей пола на Тюрских языках
-            $i=8;
-            while ($i < $amount) 
-            {
-                $item['place_of_birth'] .= $word[0][$i]." ";
-                $i++;
-            }
-        }
-        else
-        {
-            unset($item['name_second'], $item['surname_second'], $item['patronymic_second'], $item['date_of_birth'], $item['place_of_birth']);
-            $item['date_of_birth'] = $word[0][4];
-            // i=7 ибо с 7 позиции в массиве начинается место проживания, это в обычном случае ФИО.
-            $i=7;
-            while ($i < $amount) 
-            {
-                @$item['place_of_birth'] .= $word[0][$i]." ";
-                $i++;
-            }
-        }
+## Способ с использованием PHP:
+```sh
+$ apt-get update
+$ apt-get install php php-mysql
+```
 
-    }
+## Для использования вместе с HHVM:
 
-    if (timecheck($item['date_of_birth']) == false) 
-    {
-        // В случае, если у человека указано лишь фамилия и имя, а потом идет дата рождения
-        if (timecheck($word[0][3]) == false)
-        {
-            unset($item['date_of_birth'], $item['place_of_birth']);
-        }
-        else
-        {
-            unset($item['patronymic']);
-            $item['date_of_birth'] = $word[0][3];
-        }
-    }
+### Debian 8 Jessie, Debian 9 Stretch
+```sh
+$ apt-get update
+$ apt-get install -y apt-transport-https software-properties-common
+$ apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xB4112585D386EB94
 
+$ add-apt-repository https://dl.hhvm.com/debian
+$ apt-get update
+$ apt-get install hhvm
+```
 
-    // В базе террористов порой встречается формат "ФИО, ,ГОРОД"
-    // Для таких случаев следующая проверочка
-    if (strlen(@$item['date_of_birth']) < 2)
-    {
-        unset($item['date_of_birth']);
-    }
+### Ubuntu
+```sh
+$ apt-get update
+$ apt-get install software-properties-common apt-transport-https
+$ apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xB4112585D386EB94
 
-    //Для логов
-    $all[] = $item;
+$ add-apt-repository https://dl.hhvm.com/ubuntu
+$ apt-get update
+$ apt-get install hhvm
+```
+##### Дополнительную информацию вы можете найти [на официальном сайте HHVM](https://docs.hhvm.com/hhvm/installation/linux)
 
+# Использование
+### Перед использованием алгоритма стоит создать базу со следующими значениями:
+```markdown
+| name | surname | patronymic | date_of_birth | place_of_birth | name_second | surname_second | patronymic_second | ID      |
+|------|---------|------------|---------------|----------------|-------------|----------------|-------------------|---------|
+| TEXT | TEXT    | TEXT       | DATE          | TEXT           | TEXT        | TEXT           | TEXT              | INT(16) |
+```
 
-    // Для удобства
-    $list = 
-    [
-        [
-        'name'               => @$item['name'], 
-        'surname'            => trimmer(@$item['surname']),
-        'patronymic'         => trimmer(@$item['patronymic']),
-        'date_of_birth'      => trimmer(@$item['date_of_birth']),
-        'place_of_birth'     => trimmer(@$item['place_of_birth']),
-        'name_second'        => @$item['name_second'],
-        'surname_second'     => @$item['surname_second'],
-        'patronymic_second'  => trimmer(@$item['patronymic_second']),
-        'ID'                 => trimmer(@$item['ID'])
-        ],
-    ];
+##### Если используете просто php-cli:
+```sh
+$ git clone https://github.com/hugmouse/terbase.git
+$ cd terbase
+$ php terror_parser.php
+```
 
-    $sql_in = "INSERT INTO ".$ini["SERVER"]["TABLE"]."
-        (`name`, `surname`, `patronymic`, `date_of_birth`, `place_of_birth`, `name_second`, `surname_second`, `patronymic_second`, `ID`)
-    VALUES ";
+##### HHVM:
+```sh
+$ git clone https://github.com/hugmouse/terbase.git
+$ cd terbase
+$ hhvm terror_parser.php
+```
 
-    // Формируем огромный запрос!
-    foreach ($list as $key => $data) {
-        $sql_out .= 
-        "(
-            '{$data['name']}',
-            '{$data['surname']}',
-            '{$data['patronymic']}',
-            '{$data['date_of_birth']}',
-            '{$data['place_of_birth']}',
-            '{$data['name_second']}',
-            '{$data['surname_second']}',
-            '{$data['patronymic_second']}',
-            '{$data['ID']}'
-        ),";
-    }
+После установки и вызова `terror_base.php` создается файл `terrorist-base.json` , содержащий все те же данные, что передаются в базу, однако в `.json`.
 
-}
-
-// Заменяем последнюю в массиве "," на ";" и делаем запрос
-$sql_out = substr_replace($sql_out, ';', -1);
-$link->query($sql_in.$sql_out);
-
-// Для json вывода
-$result = json_encode($all);  
-file_put_contents('terrorist-base.json', $result); 
-
-// Закрываем соединение
-mysqli_close($link);
-
-printf('Обработано %d строк за %f секунд. Все удачно! %c', $data['ID'], (microtime(true) - $time_start), 10);
-?>
